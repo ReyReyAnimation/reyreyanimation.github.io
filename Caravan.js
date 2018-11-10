2f7ea6cb2106ee4deb9b8c0c00f564ee8db35bfb
ReyReyAnimation <reyreyanimation@gmail.com>
2018-11-10 20:50:19 +0000

    20181110 Enlarge Caravan

--- Caravan.js
+++ Caravan.js
@@ -3,22 +3,22 @@ function caravanMove()
   //Initialise caravan
   var elem = document.getElementById("caravanAnimation");
   elem.style.top = '150px';
-  elem.style.left = '100px';
+  elem.style.left = '25px';
   elem.style.opacity = 1.0;
   //var elemPos = 150;   
 
   //Initialse background
   var bkgd = document.getElementById("caravanBackground");
-  var bkgdPos = -290;
+  var bkgdPos = -930;
   bkgd.style.backgroundPosition = bkgdPos + "px center";
 
-  var id = setInterval(caravanFrame, 200);
+  var id = setInterval(caravanFrame, 100);
   function caravanFrame()
   {
     //Scroll background
     if (bkgdPos == 0)
     {
-      bkgdPos = -290;
+      bkgdPos = -930;
     }
     else
     {
