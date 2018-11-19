58e7a88b6416bbea099f5bf1024db106d67c6713
ReyReyAnimation <reyreyanimation@gmail.com>
2018-11-19 15:17:31 +0000

    Update Child.js

--- Child.js
+++ Child.js
@@ -3,8 +3,8 @@
 
   function contentLoaded() {
    //alert('Child: contentLoaded()');
-   idealHeight = document.getElementById('contentId').offsetHeight;
-   idealWidth = document.getElementById('contentId').offsetWidth;
+   idealHeight = document.getElementById('contentId').offsetHeight + 10;
+   idealWidth = document.getElementById('contentId').offsetWidth + 10;
    parent.contentResize(idealHeight, idealWidth);
   }
 
